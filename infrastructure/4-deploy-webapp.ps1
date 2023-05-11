b29a26889a161c7a710316253fb66d6240838c76
Piotr Rogala <71rogi@gmail.com>
2023-05-11 20:56:43 +0200

    up2

--- infrastructure/4-deploy-webapp.ps1
+++ infrastructure/4-deploy-webapp.ps1
@@ -1,5 +1,5 @@
-$studentprefix = "XXX"
-$githubAccount = "XXX"
+$studentprefix = "pr"
+$githubAccount = "rogalapiotr"
 $resourcegroupName = "fabmedical-rg-" + $studentprefix
 $webappName = "fabmedical-web-" + $studentprefix
 
